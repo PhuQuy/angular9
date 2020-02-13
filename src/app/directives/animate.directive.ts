18e546a3aed76c26f48375c5f91cabac4983005d
PhuQuy <phuquy.tool@gmail.com>
2020-02-13 11:54:16 +0700

    update animation

---
+++ src/app/directives/animate.directive.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, ElementRef, Input } from '@angular/core';
+import { ShareService } from 'app/share/share.service';
+
+export class AnimateDirective implements AfterViewInit {
+    animationInClass = '';
+    animationOutClass = '';
+
+    offsetTop;
+    screenHeight;
+    screenWidth;
+    @Input() offset = 0;
+    @Input() translateY = 0;
+    @Input() offsetMobile = 0;
+    offsetMobileLocal = 0;
+    @Input() delay = 0;
+
+    constructor(protected element: ElementRef, protected shareService: ShareService, animationInClass, animationOutClass) {
+        this.animationInClass = animationInClass;
+        this.animationOutClass = animationOutClass;
+    }
+
+    ngAfterViewInit() {
+        this.screenHeight = window.screen.height * 0.6 + this.offset;
+        this.shareService.offsetTop.subscribe(() => {
+            // this.element.nativeElement.innerHTML = this.element.nativeElement.getBoundingClientRect().top - this.screenHeight;
+            if (this.delay) {
+                setTimeout(() => {
+                    this.updateClass();
+                }, this.delay);
+            } else {
+                this.updateClass();
+            }
+        });
+    }
+
+    updateClass() {
+        if (this.element.nativeElement.getBoundingClientRect().top - this.screenHeight < 0) {
+            this.element.nativeElement.classList.add(this.animationInClass);
+            this.element.nativeElement.classList.remove(this.animationOutClass);
+        } else {
+            this.element.nativeElement.classList.remove(this.animationInClass);
+            this.element.nativeElement.classList.add(this.animationOutClass);
+        }
+    }
+}
